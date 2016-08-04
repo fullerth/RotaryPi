7047c8b5b5f3fe568c77f99132404d73fad385b1
Thomas Fuller <thomas@mcfuller.com>
2016-08-04 18:33:37 +0000

    Move config to a class. Main loop reads the correct number of edges

--- RotaryPi/RotaryPi.py
+++ RotaryPi/RotaryPi.py
@@ -2,14 +2,35 @@
 
 import RPi.GPIO as GPIO
 
+class RotaryReaderConfig():
+    def __init__(self, gpio):
+        self.GPIO_INPUT = 17 #BCM notation
+        self.GPIO_OUTPUT = 27 #BCM notation
+        self.gpio = gpio
 
-def configure_gpio(gpio):
-    GPIO_INPUT = 6 #BCM notation
-    GPIO_OUTPUT = 7 #BCM notation
-    gpio.setmode(GPIO.BCM)
-    gpio.setup(GPIO_INPUT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    gpio.setup(GPIO_OUTPUT, GPIO.OUT)
+    def configure_gpio(self):
+        self.gpio.setmode(GPIO.BCM)
+        self.gpio.setup(self.GPIO_INPUT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        self.gpio.setup(self.GPIO_OUTPUT, GPIO.OUT)
+        self.gpio.output(self.GPIO_OUTPUT, True)
 
 if __name__ == '__main__':
-    configure_gpio(GPIO)
-    print("HELLO FROM ROTARY PI")
+    r = RotaryReaderConfig(GPIO)
+    r.configure_gpio()
+    print("**READING GPIO**")
+    previous_state = 0
+    current_state = 0
+    print("initial_state: {0}".format(previous_state))
+    while True:
+        current_state = GPIO.input(r.GPIO_INPUT)
+        if(current_state != previous_state):
+            print("current_state: {0}, previous_state: {1}".format(
+                current_state, previous_state))
+        previous_state = current_state
+
+
+
+
+
+
+
