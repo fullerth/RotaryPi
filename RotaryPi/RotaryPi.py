63664c42a0e168ea184f89c38ed96e08c6b74110
Thomas Fuller <thomas@mcfuller.com>
2016-08-04 19:44:02 +0000

    Sensing dialing lines

--- RotaryPi/RotaryPi.py
+++ RotaryPi/RotaryPi.py
@@ -4,29 +4,47 @@ import RPi.GPIO as GPIO
 
 class RotaryReaderConfig():
     def __init__(self, gpio):
-        self.NUMBER_INPUT = 17 #BCM notation
-        self.NUMBER_OUTPUT = 27 #BCM notation
+        #BCM notation
+        self.NUMBER_INPUT = 17
+        self.NUMBER_OUTPUT = 27 
+        self.DIALING_INPUT = 20 
+        self.DIALING_OUTPUT = 21
         self.gpio = gpio
 
     def configure_gpio(self):
         self.gpio.setmode(GPIO.BCM)
+
+        #Setup Number Pulse Encoder IO
         self.gpio.setup(self.NUMBER_INPUT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         self.gpio.setup(self.NUMBER_OUTPUT, GPIO.OUT)
         self.gpio.output(self.NUMBER_OUTPUT, True)
 
+        #Setup Dialing Encoder IO
+        self.gpio.setup(self.DIALING_INPUT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        self.gpio.setup(self.DIALING_OUTPUT, GPIO.OUT)
+        self.gpio.output(self.DIALING_OUTPUT, True)
+
+
 if __name__ == '__main__':
+    '''Both dialing and number encoders are active low'''
     r = RotaryReaderConfig(GPIO)
     r.configure_gpio()
     print("**READING GPIO**")
-    previous_state = 0
-    current_state = 0
-    print("initial_state: {0}".format(previous_state))
+    previous_number = 0
+    current_number = 0
+    previous_dialing =  0
+    current_dialing = 0
     while True:
-        current_state = GPIO.input(r.NUMBER_INPUT)
-        if(current_state != previous_state):
-            print("current_state: {0}, previous_state: {1}".format(
-                current_state, previous_state))
-        previous_state = current_state
+        current_number = GPIO.input(r.NUMBER_INPUT)
+        current_dialing = GPIO.input(r.DIALING_INPUT)
+        if(current_dialing != previous_dialing):
+            if(current_dialing == 0):
+                print("dialing ended")
+            else:
+                print("dialing now")
+
+        previous_number = current_number
+        previous_dialing = current_dialing
 
 
 
