cc82914b38ec876f12f99fa871fc2ffcf445db37
Thomas Fuller <thomas@mcfuller.com>
2016-08-05 01:49:08 +0000

    Add Bluetooth class

--- BtKbdEmulator/BtKbdEmulator.py
+++ BtKbdEmulator/BtKbdEmulator.py
@@ -8,4 +8,13 @@ import evdev
 from evdev import *
 import keymap
 
+class Bluetooth:
+    def __init__(self):
+        # Set the device class to a keyboard and set the name
+        os.system("hciconfig hci0 class 0x002540")
+        os.system("hciconfig hci0 name RotaryPi")
+        # Make device discoverable
+        os.system("hciconfig hci0 piscan")
 
+if __name__=='__main__':
+    b = Bluetooth()
