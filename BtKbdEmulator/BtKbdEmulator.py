bc015f34d0443f4bcd02a3f4cb815075eae7a910
Thomas Fuller <thomas@mcfuller.com>
2016-08-05 04:27:58 +0000

    Adding sockets and binding to Bluetooth class

--- BtKbdEmulator/BtKbdEmulator.py
+++ BtKbdEmulator/BtKbdEmulator.py
@@ -9,12 +9,24 @@ from evdev import *
 import keymap
 
 class Bluetooth:
+    #Define ports
+    P_CTRL = 17
+    P_INTR = 19
+
     def __init__(self):
         # Set the device class to a keyboard and set the name
         os.system("hciconfig hci0 class 0x002540")
         os.system("hciconfig hci0 name RotaryPi")
         # Make device discoverable
         os.system("hciconfig hci0 piscan")
+
+        # Define server sockets for communication
+        self.scontrol = BluetoothSocket(L2CAP)
+        self.sinterrupt = BluetoothSocket(L2CAP)
+
+        # Bind the sockets to a port
+        self.scontrol.bind(("", Bluetooth.P_CTRL))
+        self.sinterrupt.bind(("", Bluetooth.P_INTR))
 
 if __name__=='__main__':
     b = Bluetooth()
